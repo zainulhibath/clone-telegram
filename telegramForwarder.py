c433be4335fb6ccaed7ffc128986f921ccf7b678
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2022-05-21 13:17:12 +0400

    Update telegramForwarder.py

--- telegramForwarder.py
+++ telegramForwarder.py
@@ -1,10 +1,10 @@
 import telegram # pip install python-telegram-bot
 
-TOKEN = "1975692196:AAFrBBttl8200RNttN0TBju1_bBjPQMn62o"  # Replace telegram bot api token here
+TOKEN = "1975696:AAFrtl8200RNttN0TBju1_bBjPQMn62o"  # Replace telegram bot api token here
 START_MSG = 9000 # Change value according your requirement
 STOP_MSG = 100000  # Change value according your requirement
 DEST_ID = "-1001343778771"  # change with your Source Channal Username/ID
-SRC_ID = "@fs_h936363_ac43dhdj8_ker57"  # change with your Destination/Backup Channal Username/ID
+SRC_ID = "@fs_h6363_ac43dhdj8_ker57"  # change with your Destination/Backup Channal Username/ID
 
 auth = telegram.Bot(token=TOKEN)
 def forward(START_MSG):
